64a7cc2b6fa0b24bfa4d3da2181d07d0280e4294
Berishev Ravil <quadraticteam@gmail.com>
2019-02-18 14:12:18 +0300

    changed scope for symbol | number | string. Added SYMBOL_SCOPE for getting current scope of class. Default_Scope changed to symbol

--- index.ts
+++ index.ts
@@ -1,21 +1,32 @@
 import * as collection from 'berish-collection';
 
-const scopes = new collection.Dictionary<string, collection.Dictionary<any, any>>();
+type KeyType = string | number | symbol;
 
-export function getSingletonInitialized<T>(classComponent: new (...args) => T, scope?: string): T {
+export const SYMBOL_SCOPE = Symbol('scope');
+const DEFAULT_SCOPE = Symbol('default');
+
+const scopes = new collection.Dictionary<KeyType, collection.Dictionary<any, any>>();
+
+export function getSingletonInitialized<T>(classComponent: new (...args) => T, scope?: KeyType): T {
+  if (scope == null) scope = DEFAULT_SCOPE;
   let singletons = getSingletonCollection(scope);
   if (singletons.containsKey(classComponent)) return singletons.get(classComponent);
   return null;
 }
 
-export function getSingleton<T>(classComponent: new (...args) => T, scope?: string, ...args): T {
+export function getSingleton<T>(classComponent: new (...args) => T, scope?: KeyType, ...args): T {
+  if (scope == null) scope = DEFAULT_SCOPE;
   let singletons = getSingletonCollection(scope);
-  if (!singletons.containsKey(classComponent)) singletons.add(classComponent, new classComponent(...args));
+  if (!singletons.containsKey(classComponent)) {
+    const instance = new classComponent(...args);
+    instance[SYMBOL_SCOPE] = scope;
+    singletons.add(classComponent, instance);
+  }
   return getSingletonInitialized(classComponent, scope);
 }
 
-export function getSingletonCollection(scope?: string) {
-  scope = scope || 'default';
+export function getSingletonCollection(scope?: KeyType) {
+  if (scope == null) scope = DEFAULT_SCOPE;
   if (!scopes.containsKey(scope)) scopes.add(scope, new collection.Dictionary());
   return scopes.get(scope);
 }
